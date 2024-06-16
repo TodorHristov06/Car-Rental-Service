31db0989abcfc305ac4ba2c80a865e2531f90fe9
Тодор Христов <todorhristov2006@gmail.com>
2024-06-16 16:06:06 +0300

    Update models

--- CarPal/Models/Vehicle.cs
+++ CarPal/Models/Vehicle.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CarPal.Models
 {
@@ -25,5 +26,8 @@ namespace CarPal.Models
         [Required]
         [StringLength(20)]
         public string Status { get; set; }
+
+        [NotMapped]
+        public string FullName => $"{Make} {Model} ({Year})";
     }
 }
