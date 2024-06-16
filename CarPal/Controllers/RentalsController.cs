4854002b96fd0c39c6ad3c0263ffbf9705c81ff7
Тодор Христов <todorhristov2006@gmail.com>
2024-06-16 15:54:33 +0300

    Add Rental, Customer controllers

---
+++ CarPal/Controllers/RentalsController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CarPal.Data;
+using CarPal.Models;
+
+namespace CarPal.Controllers
+{
+    public class RentalsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RentalsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Rentals
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Rentals.Include(r => r.Customer).Include(r => r.Vehicle);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Rentals/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rental = await _context.Rentals
+                .Include(r => r.Customer)
+                .Include(r => r.Vehicle)
+                .FirstOrDefaultAsync(m => m.RentalId == id);
+            if (rental == null)
+            {
+                return NotFound();
+            }
+
+            return View(rental);
+        }
+
+        // GET: Rentals/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "DriverLicenseNumber");
+            ViewData["VehicleId"] = new SelectList(_context.Vehicles, "VehicleId", "Make");
+            return View();
+        }
+
+        // POST: Rentals/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RentalId,VehicleId,CustomerId,StartDate,EndDate,TotalCost")] Rental rental)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(rental);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "DriverLicenseNumber", rental.CustomerId);
+            ViewData["VehicleId"] = new SelectList(_context.Vehicles, "VehicleId", "Make", rental.VehicleId);
+            return View(rental);
+        }
+
+        // GET: Rentals/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rental = await _context.Rentals.FindAsync(id);
+            if (rental == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "DriverLicenseNumber", rental.CustomerId);
+            ViewData["VehicleId"] = new SelectList(_context.Vehicles, "VehicleId", "Make", rental.VehicleId);
+            return View(rental);
+        }
+
+        // POST: Rentals/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RentalId,VehicleId,CustomerId,StartDate,EndDate,TotalCost")] Rental rental)
+        {
+            if (id != rental.RentalId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rental);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RentalExists(rental.RentalId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "DriverLicenseNumber", rental.CustomerId);
+            ViewData["VehicleId"] = new SelectList(_context.Vehicles, "VehicleId", "Make", rental.VehicleId);
+            return View(rental);
+        }
+
+        // GET: Rentals/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rental = await _context.Rentals
+                .Include(r => r.Customer)
+                .Include(r => r.Vehicle)
+                .FirstOrDefaultAsync(m => m.RentalId == id);
+            if (rental == null)
+            {
+                return NotFound();
+            }
+
+            return View(rental);
+        }
+
+        // POST: Rentals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var rental = await _context.Rentals.FindAsync(id);
+            if (rental != null)
+            {
+                _context.Rentals.Remove(rental);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RentalExists(int id)
+        {
+            return _context.Rentals.Any(e => e.RentalId == id);
+        }
+    }
+}
