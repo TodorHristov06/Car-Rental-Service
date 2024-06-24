fc00b7e7a07f8cde8f051116f994c733a9a6c782
Тодор Христов <todorhristov2006@gmail.com>
2024-06-24 20:11:39 +0300

    Add mapping

---
+++ CarPal/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarPal.ViewModels
+{
+    public class CustomerViewModel
+    {
+        public int CustomerId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string DriverLicenseNumber { get; set; }
+    }
+}
